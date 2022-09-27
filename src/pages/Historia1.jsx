07d2a8315bedc070fd424d7d3bb5078275b268f0
edu201201 <je.reyescandela@gmail.com>
2022-09-27 15:51:33 -0500

    historia 1 final (cambiar color de inputs y form

--- src/pages/Historia1.jsx
+++ src/pages/Historia1.jsx
@@ -5,30 +5,32 @@ import "bootstrap/dist/css/bootstrap.css"
 const Historia1 = () => {
     return <div classNameName="bg-dark">
         <Header />
-        <div className="bg-success container" style={{ width: "400px", paddingtop: "10px" }}>
 
-            <label className="form-label fst-italic text-white h5 " >CREATE ACCOUNT </label>
+        <form className="bg-dark container" style={{ width: "350px", paddingtop: "10px" }}>
+
+            <div className="fst-italic text-white h6 ">CREATE ACCOUNT</div>
 
             <div className="mb-3">
-                <input type="text" className="form-control" placeholder="First Name"></input>
-            </div>
-            <div className="mb-3">
-                <input type="text" className="form-control" placeholder="Last Name"></input>
-            </div>
-            <div className="mb-3">
-                <input type="email" className="form-control" placeholder="Email"></input>
-            </div>
-            <div className="mb-3">
-                <input type="password" className="form-control" placeholder="Password"></input>
+                <input type="text" className="text-white bg-dark form-control" placeholder="First Name" ></input>
             </div>
 
-            <div className="d-grid gap-2" style={{paddingbottom:"15px"}}>
+            <div className="mb-3">
+                <input type="text" className="text-white bg-dark form-control" placeholder="Last Name" ></input>
+            </div>
+
+            <div className="mb-3">
+                <input type="email" className="text-white bg-dark form-control" placeholder="Email" ></input>
+            </div>
+
+            <div className="mb-3">
+                <input type="password" className="text-white bg-dark form-control" placeholder="Password" ></input>
+            </div>
+
+            <div className="d-grid gap-2" style={{ paddingbottom: "15px" }}>
                 <button className="rounded border-0 btn-primary font-monospace h6 fs-6" style={{ padding: "5px" }} type="button">CREATE</button>
             </div>
 
-        </div>
-
-
+        </form>
 
     </div>
 
