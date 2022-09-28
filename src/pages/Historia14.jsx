2a24418f5766bfffee052e1455384ad48393801a
Geraldine191 <108587428+Geraldine191@users.noreply.github.com>
2022-09-28 18:10:28 -0500

    Esto es una prueba de uso de git

--- src/pages/Historia14.jsx
+++ src/pages/Historia14.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
+import "../assets/pc_comps"
 
 const Historia14 = () => {
     return <div className="bg-dark">
