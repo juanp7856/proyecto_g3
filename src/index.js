ed3b6c61906c19fa41c335295c3bb6e043ca80c0
edu201201 <je.reyescandela@gmail.com>
2022-09-28 14:07:28 -0500

    a

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Historia1 from './pages/Historia1';
 import Historia10 from './pages/Historia10';
 import Historia11 from './pages/Historia11';
 import Historia5 from './pages/Historia5';
+import Historia3 from './pages/Historia3';
 import Historia24 from './pages/Historia24';
 import reportWebVitals from './reportWebVitals';
 import Historia17 from './pages/Historia17';
@@ -11,7 +12,7 @@ import Historia17 from './pages/Historia17';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Historia17/>
+    <Historia3/>
   </React.StrictMode>
 );
 
