1ae1944157a2f9d197e255c0e91c5b6a60f43d22
JuanIgnacio <nachoureta2001@gmail.com>
2022-09-28 12:29:21 -0500

    historia video

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import Historia11 from './pages/Historia11';
 import Historia5 from './pages/Historia5';
 import Historia24 from './pages/Historia24';
 import reportWebVitals from './reportWebVitals';
+import Historia17 from './pages/Historia17';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Historia24/>
+    <Historia17/>
   </React.StrictMode>
 );
 
--- src/pages/Historia17.jsx
+++ src/pages/Historia17.jsx
@@ -1,11 +1,24 @@
 import React from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
+import ReactPlayer from 'react-player'
 
 const Historia17 = () => {
     return <div className="bg-dark">
         <Header />
+
+        <div className="container" style={{ marginLeft: "60px" }}>
+            <div className="card" style={{ width: "645px", height: "700px" }}>
+                <ReactPlayer url='https://www.youtube.com/watch?v=4vpPJb392Vg&feature=emb_logo'/>
+                <div className="card-body">
+                    <p className="card-text review1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+
+        </div>
     </div>
 }
+
+
 
 export default Historia17
