0f1df63e7ce19911601edb0648b903fddbd5f933
JuanIgnacio <nachoureta2001@gmail.com>
2022-09-28 10:37:57 -0500

    agregando historias

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import Historia1 from './pages/Historia1';
 import Historia10 from './pages/Historia10';
 import Historia11 from './pages/Historia11';
+import Historia5 from './pages/Historia5';
+import Historia24 from './pages/Historia24';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Historia1/>
+    <Historia24/>
   </React.StrictMode>
 );
 
