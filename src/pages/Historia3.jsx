adf5ff6ddcd247af2421c1abc461c58dbf3e5c63
edu201201 <je.reyescandela@gmail.com>
2022-09-29 00:43:33 -0500

    FINAL

--- src/pages/Historia3.jsx
+++ src/pages/Historia3.jsx
@@ -9,22 +9,23 @@ const Historia3 = () => {
         <MainHeader />
 
         <div style={{ marginLeft: "5%", marginTop: "-3%" }}>
-            <img style={{ height: "350px", width: "450px" }} src={logo} class="logo" alt="logo"/>
-        </div>
-        <div className="text-white" style={{ marginLeft: "7%", marginTop: "1%"}}>
-            <h1 className="text-white" style={{marginTop:"2%"}}>
-                Build your PC! 
-            </h1>
-            <div className="text-white" style={{marginTop:"2%"}}>
-                Just for what you need
+            <img style={{ height: "350px", width: "450px" }} src={logo} class="logo" alt="logo" />
+            <div className="text-white" style={{ marginTop: "1%" }}>
+                <h1 className="text-white" style={{ marginTop: "2%" }}>
+                    Build your PC!
+                </h1>
+                <div className="text-white" style={{ marginTop: "2%" }}>
+                    Just for what you need
+                </div>
+                <div style={{ marginTop: "2%" }}>
+                    <button type="button" className="both3 btn btn-danger border-0">Build for begginers</button>
+                    <button type="button" className="both3 btn btn-danger border-0" style={{ marginLeft: "1%" }}>Advance building</button>
+                </div>
             </div>
-            <div style={{marginTop:"2%"}}>
-                <button type="button" className="both3 btn btn-danger border-0">Build for begginers</button>
-                <button type="button" className="both3 btn btn-danger border-0" style={{marginLeft:"1%"}}>Advance building</button>
-            </div>
         </div>
-        
-        <Footer/>
+
+
+        <Footer />
     </div>
 }
 
