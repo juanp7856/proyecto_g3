a5f4c3f4e366dae400b5d1e0ad8aed8375f6690e
Jose <112113939+Joscb28@users.noreply.github.com>
2022-09-28 22:19:15 -0500

    Update Historia6.jsx

--- src/pages/Historia6.jsx
+++ src/pages/Historia6.jsx
@@ -1,10 +1,54 @@
 import React from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
+import "../styles/H6.css"
 
 const Historia6 = () => {
     return <div className="bg-dark">
         <Header />
+
+        
+        <input type="search" value="" id="search" onchange="EnterPag()"/>
+
+
+        <script>
+            function EnterPag(){
+                x=document.getElementById("search").value;
+
+                if(x==="Keyboard")
+                {
+                    window.open("./Keyboard.html")
+                }
+
+                if(x==="NVIDIA")
+                {
+                    window.open("./NVIDIA.html")
+                }
+                if(x==="COOLER")
+                {
+                    window.open("./COOLER.html")
+                }
+
+                if(x==="INTEL")
+                {
+                    window.open("./INTEL.html")
+                }
+                if(x==="NVME")
+                {
+                    window.open("./NVME.html")
+                }
+
+
+
+            }
+
+
+        </script>
+
+       
+
+
+
     </div>
 }
 
