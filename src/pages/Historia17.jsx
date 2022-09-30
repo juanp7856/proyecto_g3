bceb08ffe3c914878c22d9b3f6a4c1a27f5b388b
JuanIgnacio <nachoureta2001@gmail.com>
2022-09-30 01:46:27 -0500

    background , tratando de poner las comillas :(

--- src/pages/Historia17.jsx
+++ src/pages/Historia17.jsx
@@ -13,17 +13,24 @@ const Historia17 = () => {
         
         <div className="row" style={{ marginLeft: "120px" }}>
 
-        <div className="text-dark h1" style={{ marginBottom: "50px", marginTop:"30px" }}>Influencers</div>
+        <div className="text-dark h1" style={{ marginTop:"30px" }}>Influencers</div>
 
             <div className="card col-sm-6 border-light" style={{ width: "800px", height: "700px", marginRight:"150px", marginLeft:"300px", borderRadius:"" }}>
+                <div>
                 <ReactPlayer 
                 url='https://www.youtube.com/watch?v=4vpPJb392Vg&feature=emb_logo'
                 width='100%'
-                height='400px'
+                height='370px'
                 />
-                <div className="card-body">
-                    <p className="card-text review1 fs-5" style={{ paddingLeft:"100px", paddingRight:"100px"}}>This computer is absolutely insane! Once again if you want to check out this brand new Redux gaming PC - it's an absolute super computer. Highly, highly recommend it.</p>
-                    <div className="row">
+                </div>
+                <div className="card-body" style={{paddingTop:"50px"}}>
+                    <div className="background-autor-top">
+                        Mr Top 5.
+                    </div>
+                    <p className="card-text review1 fs-5" style={{ paddingLeft:"100px", paddingRight:"100px"}}>
+                        This computer is absolutely insane! Once again if you want to check out this brand new Redux gaming PC - it's an absolute super computer. Highly, highly recommend it.
+                    </p>
+                    <div className="row" style={{paddingTop:"20px"}}>
                         <div className="col-sm-3">
                         <img src= {MrTop5} className="MrTop5" style={{ width: "110px", height:"105px", borderRadius:"50%"}} alt="MrTop5"/>
                         </div>
@@ -42,14 +49,22 @@ const Historia17 = () => {
             </div>
 
             <div className="card col-sm-6 border-light" style={{ width: "800px", height: "700px" }}>
+                <div>
                 <ReactPlayer 
                 url='https://www.youtube.com/watch?v=UhO7MLntkDE'
                 width='100%'
-                height='400px'
+                height='370px'
                 />
-                <div className="card-body">
-                    <p className="card-text review1 fs-5" style={{ paddingLeft:"100px", paddingRight:"100px"}}>You basically just pick which games you play, which performance you want, and it'll suggest a rig for you. They make things much, much simpler and again for only $75 bucks? Okay!</p>
-                    <div className="row">
+                </div>
+
+                <div className="card-body" style={{paddingTop:"50px"}}>
+                    <div className="background-autor-short">
+                        Short Circuit
+                    </div>
+                    <p className="card-text review1 fs-5" style={{ paddingLeft:"100px", paddingRight:"100px"}}>
+                        You basically just pick which games you play, which performance you want, and it'll suggest a rig for you. They make things much, much simpler and again for only $75 bucks? Okay!
+                    </p>
+                    <div className="row" style={{paddingTop:"10px"}}>
                         <div className="col-sm-3">
                         <img src= {ShowCircuit} className="ShowCircuit" style={{ width: "110px", height:"105px", borderRadius:"50%"}} alt="ShowCircuit"/>
                         </div>
