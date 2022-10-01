2fa2860724ef403cc837c00fb328c51a16db5f23
Geraldine191 <108587428+Geraldine191@users.noreply.github.com>
2022-10-01 12:43:17 -0500

    historia 18 avance

--- src/pages/Historia18.jsx
+++ src/pages/Historia18.jsx
@@ -2,10 +2,74 @@ import React from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
 
+import headset from "../assets/periferics/headset.png"
+import mouseykeyboard from "../assets/periferics/mouseykeyboard.png"
+import standardmouse from "../assets/periferics/standardmouse.png"
+import Xlmousepad from "../assets/periferics/Xlmousepad.png"
+
 const Historia18 = () => {
     return <div className="bg-dark">
         <Header />
+        <div className="container, fs-3" style={{ marginLeft : "60px" }}> 
+            <div className="text-white" style={{ marginBottom: "30px" }}> Ranking best sellers Periferics</div>
+            
+            <div className="container">
+
+                        <div className="row">
+                            <div className="col">
+                            <p><img className="listaimg" src={headset} alt="headset" /> </p>
+                            </div>
+                            <div className="col">
+                            <p className="listatext">Headset</p>
+                            </div>
+                            <div className="col">
+                            <p className="listapr">25$</p>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col">
+                            <p><img className="listaimg" src={mouseykeyboard} alt="mouseykeyboard" /> </p>
+                            </div>
+                            <div className="col">
+                            <p className="listatext">Mouse & Keyboards</p>
+                            </div>
+                            <div className="col">
+                            <p className="listapr">39$</p>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col">
+                            <p><img className="listaimg" src={standardmouse} alt="standardmouse" /> </p>
+                            </div>
+                            <div className="col">
+                            <p className="listatext">Standard Mouse Pad</p>
+                            </div>
+                            <div className="col">
+                            <p className="listapr">19$</p>
+                            </div>
+                        </div>
+                        
+                        <div className="row">
+                            <div className="col">
+                            <p><img className="listaimg" src={Xlmousepad} alt="Xlmousepad" /> </p>
+                            </div>
+                            <div className="col">
+                            <p className="listatext">XL Mouse Pad</p>
+                            </div>
+                            <div className="col">
+                            <p className="listapr">29$</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+        </div>
     </div>
+
 }
 
 export default Historia18
